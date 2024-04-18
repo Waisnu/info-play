61239a2cc2ba86f943766c3098061ab14d75fe55
Jhonn Vincent Arcipe <freestyle2.0313@gmail.com>
2024-04-18 08:59:07 +0800

    Built color mode switch and dynamic logo for dark/light mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,18 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from '../assets/infoplay.webp'
+import { HStack, Image, useColorMode } from '@chakra-ui/react';
+import infodark from '../assets/infodark.png';
+import infolight from '../assets/infolight.png';
+import ColorModeSwitch from './ColorModeSwitch';
 
 const NavBar = () => {
-  return (
- <HStack>
-    <Image src={logo} boxSize='80px'/>
-    <Text> NavBar </Text>
- </HStack>
-  )
+    const { colorMode } = useColorMode();
+    const logoSrc = colorMode === 'dark' ? infodark : infolight;
+
+    return (
+        <HStack justifyContent='space-between' padding='15px'>
+            <Image src={logoSrc} boxSize='150px' />
+            <ColorModeSwitch />
+        </HStack>
+    );
 };
 
 export default NavBar;
--- src/components/Theme.ts
+++ src/components/Theme.ts
@@ -1,11 +1,21 @@
 import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
-    initialColorMode: '#22283',
+    initialColorMode: 'dark', // or 'dark' as per your preference
+    useSystemColorMode: false, // Set to true if you want to use system color mode
+    // Add more configuration options if needed
+    disableTransitionOnChange: false
 };
 
 const theme = extendTheme({
     config,
+    colors: {
+        // Define your custom color scheme here
+        customColorScheme: {
+            light: '#F1EFE7', // Light mode color
+            dark: '#22283F', // Dark mode color
+        },
+    },
 });
 
 export default theme;
