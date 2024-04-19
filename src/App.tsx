fe85ffe122659491ba1aa825c90649ce3107f538
Jhonn Vincent Arcipe <freestyle2.0313@gmail.com>
2024-04-19 09:33:39 +0800

    Add GenreList component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react';
 import NavBar from './components/NavBar';
 import GameGrid from './components/GameGrid';
+import GenreList from './components/GenreList';
 
 function App() {
     return (
@@ -10,12 +11,12 @@ function App() {
                 lg: `"nav nav" "aside main"`,
             }}
         >
-            <GridItem area='nav' >
+            <GridItem area='nav'>
                 <NavBar />
             </GridItem>
             <Show above='lg'>
                 <GridItem area='aside' bg='gold'>
-                    Aside
+                    <GenreList />
                 </GridItem>
             </Show>
             <GridItem area='main'>
@@ -25,4 +26,3 @@ function App() {
     );
 }
 export default App;
- 
