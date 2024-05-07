3bc792bba76c5d7f17c3c4371d4c54fc6f505a05
Jhonn Vincent Arcipe <freestyle2.0313@gmail.com>
2024-05-08 02:13:45 +0800

    implement search

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
             }}
         >
             <GridItem area='nav'>
-                <NavBar />
+                <NavBar onSearch={(searchText) => setGameQuery ({...gameQuery, searchText})} />
             </GridItem>
             <Show above='lg'>
                 <GridItem area='aside' paddingX={5}>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,15 +3,17 @@ import infodark from '../assets/infodark.png';
 import infolight from '../assets/infolight.png';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
-
-const NavBar = () => {
+interface Props {
+    onSearch: (searchText: string) => void;
+}
+const NavBar = ({ onSearch}: Props ) => {
     const { colorMode } = useColorMode();
     const logoSrc = colorMode === 'dark' ? infodark : infolight;
 
     return (
         <HStack  padding='15px'>
             <Image src={logoSrc} boxSize='150px' />
-            <SearchInput />
+            <SearchInput onSearch={onSearch} />
             <ColorModeSwitch />
         </HStack>
     );
